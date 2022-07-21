163c56158ae5f0587a5edee6423b1f111f932084
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-07-21 18:28:30 +0530

    Sidebar highlighters

--- components/LeftSideBar.vue
+++ components/LeftSideBar.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="flex w-full flex-col">
-    <div class="flex flex-col" :key="item" v-for="(item, value, index) in listingItems">
+    <div class="flex flex-col" :key="value" v-for="(item, value, index) in listingItems">
       <h2 class="text-white text-lg font-medium" :class="{ 'mt-10': index > 0 }">
         {{ value }}
       </h2>
       <NuxtLink :key="subItem.name" :to="subItem.route" v-for="subItem in item">
-        <h3 class="text-md mt-2 font-light text-[#FFFFFF75]">{{ subItem.name }}</h3>
+        <h3
+          class="text-md mt-2"
+          :class="{ 'font-medium text-[#FFFFFF]': $route.path === subItem.route, 'font-light text-[#FFFFFF75]': $route.path !== subItem.route }"
+        >
+          {{ subItem.name }}
+        </h3>
       </NuxtLink>
     </div>
   </div>
@@ -18,26 +23,20 @@ export default {
       listingItems: {
         'All Categories': [
           {
-            name: 'Apparel',
-            route: '/commerce/apparel',
+            name: 'Joggers',
+            route: '/commerce/joggers',
+          },
+          {
+            name: 'Jackets',
+            route: '/commerce/jackets',
+          },
+          {
+            name: 'T-Shirts',
+            route: '/commerce/t-shirts',
           },
           {
             name: 'Shop All',
             route: '/commerce/shop-all',
-          },
-        ],
-        'All Designers': [
-          {
-            name: 'Sagaform',
-            route: '/commerce/sagaform',
-          },
-          {
-            name: 'OFS',
-            route: '/commerce/ofs',
-          },
-          {
-            name: 'ACME',
-            route: '/commerce/acme',
           },
         ],
       },
--- components/RightSideBar.vue
+++ components/RightSideBar.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="flex w-full flex-col">
-    <div class="flex flex-col" :key="item" v-for="(item, value, index) in listingItems">
+    <div class="flex flex-col" :key="value" v-for="(item, value, index) in listingItems">
       <h2 class="text-white text-lg font-medium" :class="{ 'mt-10': index > 0 }">
         {{ value }}
       </h2>
-      <NuxtLink :key="subItem.name" :to="subItem.route" v-for="subItem in item">
-        <h3 class="text-md mt-2 font-light text-[#FFFFFF75]">{{ subItem.name }}</h3>
-      </NuxtLink>
+      <a
+        :key="subItem.name"
+        v-for="subItem in item"
+        @click="pushFilter(subItem.filter)"
+        class="text-md mt-2 cursor-pointer"
+        :class="{
+          'font-medium text-[#FFFFFF]': $route.query.filter === subItem.filter,
+          'font-light text-[#FFFFFF75]': $route.query.filter !== subItem.filter,
+        }"
+      >
+        {{ subItem.name }}
+      </a>
     </div>
   </div>
 </template>
@@ -19,23 +28,24 @@ export default {
         Relevance: [
           {
             name: 'Trending',
-            route: 'trending',
-          },
-          {
-            name: 'Latest Arrivals',
-            route: 'latest-arrivals',
+            filter: 'trending',
           },
           {
             name: 'Price: Low to High',
-            route: 'price-low-to-high',
+            filter: 'price-low-to-high',
           },
           {
             name: 'Price: High to Low',
-            route: 'price-high-to-low',
+            filter: 'price-high-to-low',
           },
         ],
       },
     }
+  },
+  methods: {
+    pushFilter(filter) {
+      this.$router.push({ path: this.$route.path, query: { filter } })
+    },
   },
 }
 </script>
