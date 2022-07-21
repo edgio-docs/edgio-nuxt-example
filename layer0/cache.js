4da58f369acc3d37ff4297e0a088c6e3586e6912
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-07-21 18:28:37 +0530

    Image API

--- layer0/cache.js
+++ layer0/cache.js
@@ -26,6 +26,25 @@ export const EDGE_CACHE_HANDLER = ({ cache }) => {
   })
 }
 
+export const IMAGE_CACHE_HANDLER = ({ cache, proxy }) => {
+  cache({
+    edge: {
+      maxAgeSeconds: 60 * 60,
+      // Cache responses even if they contain cache-control: private header
+      // https://docs.layer0.co/guides/caching#private
+      // https://docs.layer0.co/docs/api/core/interfaces/_router_cacheoptions_.edgecacheoptions.html#forceprivatecaching
+      forcePrivateCaching: true,
+    },
+    browser: {
+      // Don't save the response in the browser
+      maxAgeSeconds: 0,
+      // Save the response in the browser via Layer0 service worker
+      serviceWorkerSeconds: 60 * 60 * 24,
+    },
+  })
+  proxy('image', { path: '/' })
+}
+
 export const STATIC_CACHE_CONFIG = {
   browser: {
     serviceWorkerSeconds: 60 * 60 * 24 * 365,
