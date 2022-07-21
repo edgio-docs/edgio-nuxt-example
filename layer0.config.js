21558852860a634477b7f5c26b8c1c9fcdc6a567
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-07-21 18:27:57 +0530

    Image Optimization

--- layer0.config.js
+++ layer0.config.js
@@ -1,9 +1,9 @@
-const includeFiles = require('./layer0/getNodeModules')
+const { isProductionBuild } = require('@layer0/core/environment')
 
 // This file was automatically added by layer0 init.
 // You should commit this file to source control.
 // Learn more about this file at https://docs.layer0.co/guides/layer0_config
-module.exports = {
+const config = {
   routes: './layer0/routes.js',
   connector: '@layer0/nuxt',
   backends: {
@@ -16,7 +16,19 @@ module.exports = {
       // https://docs.layer0.co/guides/layer0_config#:~:text=browser%20is%20used.-,disableCheckCert,-Boolean
       disableCheckCert: true,
     },
+    // More on: https://docs.layer0.co/guides/image_optimization
+    image: {
+      domainOrIp: 'opt.moovweb.net',
+      hostHeader: 'opt.moovweb.net',
+      disableCheckCert: true,
+    },
   },
-  // All dependencies inside @nuxt/core
-  includeFiles,
 }
+
+if (isProductionBuild()) {
+  const includeFiles = require('./layer0/getNodeModules')
+  // All dependencies inside @nuxt/core
+  config['includeFiles'] = includeFiles
+}
+
+module.exports = config
